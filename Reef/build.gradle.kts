0c01709509974094298971e46a0b7bab6c23f6ae
alv-cor <github.carport038@passmail.com>
2026-01-29 14:15:11 +0100

    Bump app version to 3.0.8 and update .gitignore
    Updated versionCode to 308 and versionName to 3.0.8 in build.gradle.kts. Added /.kotlin/sessions to .gitignore to exclude Kotlin session files from version control.

--- Reef/build.gradle.kts
+++ Reef/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.pranav.reef"
         minSdk = 33
         targetSdk = 36
-        versionCode = 307
-        versionName = "3.0.7"
+        versionCode = 308
+        versionName = "3.0.8"
     }
 
     viewBinding.enable = true
